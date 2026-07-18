d6d9da04adfb2b0432d7dfd0da8be650383933fd
sadhubby <evanm.deguzman@gmail.com>
2026-07-19 01:17:05 +0800

    ui: step missing

--- client/ui.py
+++ client/ui.py
@@ -324,7 +324,7 @@ def draw_instruction_panel(surface, area):
         vec_h = 2 * pf.get_height() + 2
 
         step_colors = {None: None, "firstStep": None, "firstStepOne": [config.COLOR_HIGHLIGHT_BLUE, config.COLOR_POINT_LIGHT], "firstStepTwo": [config.COLOR_HIGHLIGHT_BLUE, config.COLOR_POINT_LIGHT], "firstStepThree": [config.COLOR_HIGHLIGHT_BLUE, config.COLOR_POINT_LIGHT]}
-        hl_u = step_colors.get(config.current_step)
+        hl_u = step_colors.get(config.current_step) if config.current_step == "firstStepTwo" else None
         hl_w = step_colors.get(config.current_step) if config.current_step == "firstStepThree" else None
         hl_v = step_colors.get(config.current_step) if config.current_step == "firstStepOne" else None
 
