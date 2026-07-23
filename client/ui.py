12a735fe9b1544f292fbf1a57f080585fe742bce
edg <122878019+sadhubby@users.noreply.github.com>
2026-07-23 12:58:20 +0800

    ui: indetation

--- client/ui.py
+++ client/ui.py
@@ -695,7 +695,7 @@ def draw_projection_boxes(surface, center):
     if highlight_step in config.PROJECTION_REGIONS:
         proj_mat = track_mat if tracking else frozen_mat
         if proj_mat is not None:
-                if config.args.mode == "highlights" and not config.blank_projection and fb_timer <= 0:
+            if config.args.mode == "highlights" and not config.blank_projection and fb_timer <= 0:
                 for _, box in config.PROJECTION_REGIONS[highlight_step].items():
                     tl = transform_to_projection_space(
                         box["left"], box["top"], center, proj_mat)
